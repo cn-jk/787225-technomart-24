0d05d59e9bfb30dc2e87060ecb3d98685aa0547f
cn-jk <cn_jk@mail.ru>
2019-03-20 23:59:22 -0700

    Вариант проекта "Technomart" для защиты (улучшенный).
    --787225--
    
    21-03-2019

--- js/bookmark-korzina-red.js
+++ js/bookmark-korzina-red.js
@@ -6,53 +6,52 @@ var input_Value;
 
 // .........................
 
-for ( i = 0; i < node_Link.length; i++ )
+for ( var i = 0; i < node_Link.length; i++ )
 {
   fun_ChangeColor( node_Link[i].id );
 }
 
 // .........................
 
-  fun_ChangeColor_2('id-search-form');
+//  fun_ChangeColor_2('id-search-form');
 
-// :::::::::::::::::::::::::
+// :::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::
 
 function fun_ChangeColor( id_element )
 {
   
-//  var parentNode_ChangeColor = document.getElementById( id_element );
   var node_ChangeColor = document.getElementById( id_element );
-
-  var tag_for_Work     = 'input';
-  var class_for_Work   = 'user-korzina-red';
-
-  var input_ChangeColor;
-  var input_Value;
+  
+  var class_for_Work      = '.tag-number';  //  var tag_for_Work = 'input';
+  var class_ChangeColor = 'user-korzina-red';
+  
+  var tag_ChangeColor;  //  var input_ChangeColor;
+  var tag_Value;        //  var input_Value;
   
 // .........................
   
 try
   {
-    input_ChangeColor = node_ChangeColor.querySelector(tag_for_Work);
+    tag_ChangeColor = node_ChangeColor.querySelector(class_for_Work);
   }
 catch (err)
   {
     return;
   }
   
-  input_Value = input_ChangeColor.value;
+  tag_Value = tag_ChangeColor.innerHTML;  // tag_Value = tag_ChangeColor.value;
     
-if ( input_Value * 1  >  0 )
+if ( tag_Value * 1  >  0 )
   {
-    node_ChangeColor.classList.add(class_for_Work);
+    node_ChangeColor.classList.add(class_ChangeColor);
   }
 else
   {
-    node_ChangeColor.classList.remove(class_for_Work);
+    node_ChangeColor.classList.remove(class_ChangeColor);
   }
 }
 
-// :::::::::::::::::::::::::
+// :::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::
 
 function fun_ChangeColor_2( id_element )
 {
@@ -79,12 +78,65 @@ catch (err)
   
 if ( input_Value.value )
   {
-    node_ChangeColor.classList.add(class_for_Work);
+    node_ChangeColor.classList.add( class_for_Work);
+    input_Value.classList.add('inpt-search-white');
   }
 else
   {
     node_ChangeColor.classList.remove(class_for_Work);
+    input_Value.classList.remove('inpt-search-white');
   }  
 }
 
+// :::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::
+
+function fun_ChangeColor_3( id_element )
+{
+
+  var parentNode_for_Work = document.getElementById( id_element );
+  
+  var node_ChangeColor;
+
+  var tag_ChangeColor  = 'label';
+  var class_for_Work   = 'icon-search-red';
+
+// .........................
+  
+try
+  {
+    node_ChangeColor = parentNode_for_Work.querySelector(tag_ChangeColor);  
+  }
+catch (err)
+  {
+    return;
+  }
+  
+  node_ChangeColor.classList.add( class_for_Work);
+}
+
+// :::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::
+
+function fun_ChangeColor_4( id_element )
+{
+
+  var parentNode_for_Work = document.getElementById( id_element );
+  
+  var node_ChangeColor;
+
+  var tag_ChangeColor  = 'label';
+  var class_for_Work   = 'icon-search-red';
+
+// .........................
+  
+try
+  {
+    node_ChangeColor = parentNode_for_Work.querySelector(tag_ChangeColor);  
+  }
+catch (err)
+  {
+    return;
+  }
+  
+  node_ChangeColor.classList.remove(class_for_Work);
+}
 // :::::::::::::::::::::::::
